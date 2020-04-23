7c854b79536701e0f43252b77179a22c7271a370
Dennis97 <dinishlypovka@gmail.com>
2020-04-23 12:01:27 +0200

    refactoring

--- src/main/java/org/exchangerates/controller/HTMLController.java
+++ src/main/java/org/exchangerates/controller/HTMLController.java
@@ -1,6 +1,7 @@
 package org.exchangerates.controller;
 
 import org.exchangerates.model.BankType;
+import org.exchangerates.model.Currency;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,8 @@ public class HTMLController {
 
   @GetMapping("/home")
   public String getIndex(Model model) {
-    model.addAttribute("banksList", BankType.values());
+    model.addAttribute("banks", BankType.values());
+    model.addAttribute("currencies", Currency.values());
     return "index";
   }
 }
